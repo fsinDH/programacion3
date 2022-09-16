e21b4ad9cf1aa9817dbff57e49915c5e1f650e0d
RoniGottfried <rgottfried@edu.ar>
2022-09-16 18:19:01 -0300

    filtrado de datos del input

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react'; 
 import "./header.css"
+import Card from '../../components/Card/Card'
 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto&display=swap" rel="stylesheet"></link>
 
-export default function Header() {
+export default class Header extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            filterBy:'',
+            resultados:[],
+        }
+        console.log (this.props)
+        
+    }
+
+    PeliculasFiltradas(palabra){
+        if (palabra === ""){
+            return
+        } else {
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=2a48645239a84bc2697fe8c66fc6dc00&language=en-US&query=${palabra}`
+        
+        fetch(url)
+            .then((res)=> res.json())
+            .then(datos =>{ 
+                console.log(datos)
+                return this.setState({
+                resultados: datos.results,
+            })
+        })
+        .catch( err => console.log(err))}
+    }
+
+    handleChange(e){
+        this.setState({
+          filterBy: e.target.value
+        },()=>{
+          this.PeliculasFiltradas(this.state.filterBy)
+        })
+    }
+
+
+    render(){
     return (
     <>
         <header className="header">
@@ -36,16 +76,21 @@ export default function Header() {
                         <div class="search"> 
                             <div>
                                 <form className='formBusqueda' method="GET">
-                                    <input type="text" placeholder="Search . . ." required/>
+                                    <input type="text" onChange={(e)=>{this.handleChange(e)}} value={this.state.filterBy} placeholder="Search . . ." required/>
                                 </form>
                             </div>
                         </div>
                     </div>
-
+                    
+                        
             </div>
-
+                
         </header>
+        {/* Hace que el form filtre por los buscados */}
+            { this.state.cargando === false ? (<p>Cargando</p>) : 
+            (this.state.resultados.map(resultado => (<Card key={resultado.id} pelicula={resultado}/>)))} 
     </>
     )
+    }
 }
 
